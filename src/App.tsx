b6f3c1f3911cbea8c117b538276d7f621dd4b692
mehran <mehranmohamadi1311@gmail.com>
2025-12-05 23:46:17 +0330

    feat: add StarRating component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import StarRating from "./StarRating";
+
 function App() {
-  return <div>StarRating</div>;
+  return (
+    <div className="h-dvh bg-slate-500 pt-24    ">
+      <div className="flex flex-col space-y-3 items-start mx-auto w-fit">
+        <StarRating />
+      </div>
+    </div>
+  );
 }
 
 export default App;
